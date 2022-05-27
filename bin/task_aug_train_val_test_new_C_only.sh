1db87c282293ae0d30940f142273905c7c8b8d14
yu-fang <y.f.10010s@gmail.com>
2022-05-27 10:19:53 +0800

    modify scripts of task aug

--- bin/task_aug_train_val_test_new_C_only.sh
+++ bin/task_aug_train_val_test_new_C_only.sh
@@ -35,22 +35,40 @@ n_test_class=16
 # n_val_class=5
 # n_test_class=11
 
-for way_shot in '5way_1shot' '5way_5shot'
+generate='nli-generator'
+csv_path='task_aug_train_val_test_new'
+for way_shot in '5way-1shot' '5way-5shot'
 do
-    for data_path in "data/aug_all_roberta_select_huffpost_G1_10N_top-k_40.json"
+    if [ "$way_shot" = '5way-1shot' ]; then
+        way=5
+        shot=1
+    elif [ "$way_shot" = '5way-5shot' ]; then
+        way=5
+        shot=5
+    fi
+
+    for data_path in 'data/aug_all_roberta_select_huffpost_G1_10N_top-k_40.json'
     do
         r=0
         if [ "$data_path" = "data/aug_all_roberta_select_huffpost_G1_10N_top-k_40.json" ]; then
-            generate='nli-gernerator'
-        elif [ "$data_path" = "pass" ]; then
-            generate='t5-large'
+            DA_name="C_only"
+        # elif [ "$data_path" = "data/t5-large_huffpost_roberta-large-mnli_10N_top-k_40_EorN.json" ]; then
+        #     DA_name="EorN"
+        # elif [ "$data_path" = "data/t5-large_huffpost_roberta-large-mnli_10N_top-k_40_N_only.json" ]; then
+        #     DA_name="N_only"
+        # elif [ "$data_path" = "data/huffpost_double_text.json" ]; then
+        #     DA_name="double_text"
         fi
 
         for seed in 42 80 100 200 300
         do
             ((r++))
-            result_path='result/task_aug_train_val_test_new_C_only_'$way_shot'_'$generate'_'$r
-        
+            if [ "$DA_name" = "double_text" ]; then
+                result_path='result/'$csv_path'_'$way_shot'_'$DA_name'_'$r
+            else
+                result_path='result/'$csv_path'_'$way_shot'_'$generate'_'$DA_name'_'$r
+            fi
+
             if [ "$dataset" = "fewrel" ]; then
                 python src/main.py \
                     --cuda 0 \
@@ -71,8 +89,8 @@ do
             else
                 python src/main.py \
                     --cuda １ \
-                    --way 5 \
-                    --shot 1 \
+                    --way=$way \
+                    --shot=$shot \
                     --query 25 \
                     --mode train \
                     --embedding meta \
@@ -88,6 +106,7 @@ do
                     --task_aug_target train_val \
                     --test_new_only \
                     --result_path=$result_path \
+                    --csv_path=$csv_path
                     --seed=$seed
             fi
         done
